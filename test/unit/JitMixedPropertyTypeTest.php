9cc9c7802a5c303681a9afdf45571f0a734a5379
PurHur <PurHur@users.noreply.github.com>
2026-05-23 18:49:14 +0200

    Add unit test for mixed property JIT type mapping (#1086)

---
+++ test/unit/JitMixedPropertyTypeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPCompiler\Test\Unit;
+
+use PHPTypes\Type;
+use PHPCompiler\JIT\Variable as JitVariable;
+use PHPUnit\Framework\TestCase;
+
+final class JitMixedPropertyTypeTest extends TestCase
+{
+    public function testMixedDocblockMapsToValueBoxType(): void
+    {
+        $mixed = Type::mixed();
+
+        $this->assertSame(JitVariable::TYPE_VALUE, JitVariable::getTypeFromType($mixed));
+    }
+}
